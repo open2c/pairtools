3a6dc2885dcb80b1ecb99810ccfa3f910e349d78
Anton Goloborodko <goloborodko.anton@gmail.com>
2017-02-24 17:19:56 -0500

    distiller.sh - use the new names

--- distiller.sh
+++ distiller.sh
@@ -6,7 +6,7 @@ OUTNAME=$4
 ~/programming/bwa/bwa mem -SP $INDEX $FASTQ1 $FASTQ2 | {
 # save unmapped/single-sided/multimapped/abnormal chimeras 
 # and output lines with flipped pairs and the two sam entries, separated by \v
-    python hisplitter.py \
+    python classify_sam.py \
         --header $OUTNAME.header.sam \
         --unmapped >(samtools view -bS - > $OUTNAME.unmapped.bam) \
         --singlesided >(samtools view -bS - > $OUTNAME.singlesided.bam) \
@@ -18,13 +18,13 @@ OUTNAME=$4
     sort -k 1,1 -k 4,4 -k 2,2n -k 5,5n --field-separator=\v
 } | {
 # remove duplicates and split pairs and sams
-    python dedup.py \
-        --out >(python split_pairs.py \
+    python dedup_pairsam.py \
+        --out >(python split_pairsam.py \
                 --header $OUTNAME.header.sam \
                 --out-pairs $OUTNAME.dedup.pairs \
                 --out-sam >(samtools view -bS - > $OUTNAME.dedup.bam)
         ) \
-        --dupfile >(python split_pairs.py \
+        --dupfile >(python split_pairsam.py \
                 --header $OUTNAME.header.sam \
                 --out-pairs /dev/null \
                 --out-sam >(samtools view -bS - > $OUTNAME.dups.bam)
