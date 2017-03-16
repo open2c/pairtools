f31a617b12fedc6cf672f00426073ccd8f715384
golobor <goloborodko.anton@gmail.com>
2017-03-16 16:38:50 -0400

    distiller.sh: change paths

--- distiller.sh
+++ distiller.sh
@@ -3,7 +3,7 @@ FASTQ1=$2
 FASTQ2=$3                                                  
 OUTPREFIX=$4
 
-~/programming/bwa/bwa mem -SP "$INDEX" "$FASTQ1" "$FASTQ2" | {
+bwa mem -SP "$INDEX" "$FASTQ1" "$FASTQ2" | {
 # classify Hi-C molecules as unmapped/single-sided/multimapped/chimeric/etc
 # and output one line per read, containing flipped pairs, type of a Hi-C molecule
 # and the corresponding sam entries, all separated by \v
