acd65d171a6c0c1a07617ecf5904713f990b478a
golobor <goloborodko.anton@gmail.com>
2017-03-16 16:36:17 -0400

    distiller.sh: bugfix

--- distiller.sh
+++ distiller.sh
@@ -1,9 +1,9 @@
 INDEX=$1                                                   
 FASTQ1=$2                                                  
 FASTQ2=$3                                                  
-OUTNAME=$4
+OUTPREFIX=$4
 
-~/programming/bwa/bwa mem -SP $INDEX $FASTQ1 $FASTQ2 | {
+~/programming/bwa/bwa mem -SP "$INDEX" "$FASTQ1" "$FASTQ2" | {
 # classify Hi-C molecules as unmapped/single-sided/multimapped/chimeric/etc
 # and output one line per read, containing flipped pairs, type of a Hi-C molecule
 # and the corresponding sam entries, all separated by \v
@@ -13,6 +13,6 @@ OUTNAME=$4
     bash sort_pairsam.sh
 } | {
 # remove duplicates and split pairs and sams
-    bash dedup_split_pairsam.sh
+    bash dedup_split_pairsam.sh "$OUTPREFIX"
 }
 
