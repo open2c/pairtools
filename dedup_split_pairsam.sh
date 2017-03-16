8a51f1392adf257e1ca599da22b8c6ee99d4eab1
golobor <goloborodko.anton@gmail.com>
2017-03-16 15:45:34 -0400

    the first working prototype

---
+++ dedup_split_pairsam.sh
@@ -0,0 +1,23 @@
+PREFIX=$1
+
+UNMAPPED_SAM_PATH=${PREFIX}.unmapped.bam
+UNMAPPED_PAIRS_PATH=${PREFIX}.unmapped.pairs.gz
+NODUPS_SAM_PATH=${PREFIX}.nodups.bam
+NODUPS_PAIRS_PATH=${PREFIX}.nodups.pairs.gz
+DUPS_SAM_PATH=${PREFIX}.dups.bam
+DUPS_PAIRS_PATH=${PREFIX}.dups.pairs.gz
+
+cat - | \
+    bash split_unmapped.sh \
+        >( python split_pairsam.py \
+            --out-pairs ${UNMAPPED_PAIRS_PATH} \
+            --out-sam ${UNMAPPED_SAM_PATH} ) | \
+    python dedup.py  \
+        --out \
+            >( python split_pairsam.py \
+                --out-pairs ${NODUPS_PAIRS_PATH} \
+                --out-sam ${NODUPS_SAM_PATH} )\
+        --dupfile \
+            >( python tag_duplicates.py | python split_pairsam.py \
+                --out-pairs ${DUPS_PAIRS_PATH} \
+                --out-sam ${DUPS_SAM_PATH} )
--- distiller.sh
+++ distiller.sh
@@ -1,28 +1,17 @@
 INDEX=$1                                                   
 FASTQ1=$2                                                  
 FASTQ2=$3                                                  
-OUTNAME=$4                                                 
+OUTNAME=$4
 
 ~/programming/bwa/bwa mem -SP $INDEX $FASTQ1 $FASTQ2 | {
 # save unmapped/single-sided/multimapped/abnormal chimeras 
 # and output lines with flipped pairs and the two sam entries, separated by \v
-    python classify_sam.py \
-        --out-basename $OUTNAME 
+    python classify_sam.py 
 } | {
 # lexicographic block-sort pairs together with sam entries
-    sort -k 1,1 -k 4,4 -k 2,2n -k 5,5n --field-separator=\v
+    bash sort_pairsam.sh
 } | {
 # remove duplicates and split pairs and sams
-    python dedup_pairsam.py \
-        --out >(python split_pairsam.py \
-                --header $OUTNAME.header.bam \
-                --out-pairs $OUTNAME.dedup.pairs \
-                --out-sam $OUTNAME.dedup.bam
-        ) \
-        --dupfile >(python split_pairsam.py \
-                --header $OUTNAME.header.bam \
-                --out-pairs /dev/null \
-                --out-sam $OUTNAME.dups.bam
-        )
+    bash dedup_split_pairsam.sh
 }
 
