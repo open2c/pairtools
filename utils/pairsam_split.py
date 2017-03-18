758a045a2d9c7d4d3518c2d32da960180ac4d4d0
golobor <goloborodko.anton@gmail.com>
2017-03-17 22:01:45 -0400

    fix python code: create a library of common functions, edit merge

--- utils/pairsam_split.py
+++ utils/pairsam_split.py
@@ -3,6 +3,7 @@
 import argparse
 import pipes
 import sys
+from _distiller_common import open_bam_or_sam, open_bgzip
 
 
 def main():
@@ -60,44 +61,6 @@ def main():
 
     if hasattr(sam_file, 'close'):
         sam_file.close()
-
-
-def open_sam_or_bam(path, mode):
-    if mode not in ['r','w']:
-        raise Exception("mode can be either 'r' or 'w'")
-    if path.endswith('.bam'):
-        if mode =='w': 
-            t = pipes.Template()
-            t.append('samtools view -bS', '--')
-            f = t.open(path, 'w')
-        elif mode =='r': 
-            t = pipes.Template()
-            t.append('samtools view -h', '--')
-            f = t.open(path, 'r')
-        else:
-            raise Exception("Unknown mode : {}".format(mode))
-        return f
-    else:
-        return open(path, mode)
-
-
-def open_bgzip(path, mode):
-    if mode not in ['r','w']:
-        raise Exception("mode can be either 'r' or 'w'")
-    if path.endswith('.gz'):
-        if mode =='w': 
-            t = pipes.Template()
-            t.append('bgzip -c', '--')
-            f = t.open(path, 'w')
-        elif mode =='r': 
-            t = pipes.Template()
-            t.append('zcat', '--')
-            f = t.open(path, 'r')
-        else:
-            raise Exception("Unknown mode : {}".format(mode))
-        return f
-    else:
-        return open(path, mode)
 
 
 if __name__ == '__main__':
