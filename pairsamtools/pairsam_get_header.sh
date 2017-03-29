d260fd9dea5cf6869704d8192d006201f52cc6c6
golobor <goloborodko.anton@gmail.com>
2017-03-29 13:02:20 -0400

    drop bash tools

--- pairsamtools/pairsam_get_header.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -ge 1 -a -f "$1" ] ; then
-    awk '{if(/^#/)print;else exit}' $1
-else 
-    awk '{if(/^#/)print;else exit}'
-fi 
--- pairsamtools/pairsam_skip_header.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -ge 1 -a -f "$1" ] ; then
-    sed -n -e '/^[^#]/,$p' $1
-else 
-    sed -n -e '/^[^#]/,$p'
-fi 
-
