785e713210b7c9a2a2142e3899cde9e9b5ed5ea8
sergpolly <sergpolly@gmail.com>
2017-07-10 14:03:32 -0400

    merge options expanded with threads and batches

--- pairsamtools/pairsam_merge.py
+++ pairsamtools/pairsam_merge.py
@@ -24,13 +24,50 @@ UTIL_NAME = 'pairsam_merge'
         ' If the path ends with .gz, the output is bgzip-compressed.'
         ' By default, the output is printed into stdout.')
 @click.option(
-    "--nproc", 
+    "--npzipin", 
     type=int, 
-    default=8, 
-    help='Number of threads per process.',
+    default=2, 
+    help='Number of threads per pbgzip instance in the input stream (decompression).'
+        ' Decompression of all individual chunks starts simultaneously,'
+        ' so, take into account total number of merging chunks to adjust this flag',
     show_default=True,
     )
-def merge(pairsam_path, output, nproc):
+@click.option(
+    "--npzipout", 
+    type=int, 
+    default=4, 
+    help='Number of threads per pbgzip instance in the output stream (compression).',
+    show_default=True,
+    )
+@click.option(
+    "--npmerge", 
+    type=int, 
+    default=2, 
+    help='Number of threads per sort --merge.',
+    show_default=True,
+    )
+@click.option(
+    "--batchmerge", 
+    type=int, 
+    default=0, 
+    help='Merging batch size for sort --merge --batch-size'
+        ' Default value implies all chunks are merged together simultaneously in a single batch (no batching)'
+        ' This flag controls the batch size only.'
+        ' It does not control the number of batches being merged simultaneously'
+        ' This flag does not control the number of input decompression streams in any way.',
+    show_default=True,
+    )
+@click.option(
+    "--tempdir", 
+    type=str, 
+    default=".", 
+    help='Path to store temporary files that sort --merge generates'
+        ' Use it only with batchmerge flag.',
+    )
+
+
+
+def merge(pairsam_path, output, npzipin, npzipout, npmerge, batchmerge, tempdir):
     """merge sorted pairs/pairsam files. 
 
     Merge triu-flipped sorted pairs/pairsam files. If present, the @SQ records 
@@ -44,13 +81,13 @@ def merge(pairsam_path, output, nproc):
     or a group/groups of .pairsam files specified by a wildcard
     
     """
-    merge_py(pairsam_path, output, nproc)
+    merge_py(pairsam_path, output, npzipin, npzipout, npmerge, batchmerge, tempdir)
 
 
-def merge_py(pairsam_path, output, nproc):
+def merge_py(pairsam_path, output, npzipin, npzipout, npmerge, batchmerge, tempdir):
     paths = sum([glob.glob(mask) for mask in pairsam_path], [])
 
-    outstream = (_io.open_bgzip(output, mode='w', nproc=nproc) 
+    outstream = (_io.open_bgzip(output, mode='w', nproc=npzipout) 
                  if output else sys.stdout)
 
     headers = []
@@ -70,6 +107,8 @@ def merge_py(pairsam_path, output, nproc):
         /bin/bash -c 'sort -k {0},{0} -k {1},{1} -k {2},{2}n -k {3},{3}n -k {4},{4} 
         --merge  --field-separator=$'\''{5}'\''
         {6}
+        {7}
+        {8}
         -S 1G
         '''.replace('\n',' ').format(
                 _pairsam_format.COL_C1+1, 
@@ -78,11 +117,13 @@ def merge_py(pairsam_path, output, nproc):
                 _pairsam_format.COL_P2+1,
                 _pairsam_format.COL_PTYPE+1,
                 _pairsam_format.PAIRSAM_SEP_ESCAPE,
-                ' --parallel={} '.format(nproc) if nproc > 1 else ' ',
+                ' --parallel={} '.format(npmerge) if npmerge > 1 else ' ',
+                ' --temporary-directory={}'.format(tempdir),
+                ' --batch-size={}'.format(batchmerge) if batchmerge > 1 else ' ',
                 )
     for path in paths:
         if path.endswith('.gz'):
-            command += r''' <(pbgzip -dc -n {} {} | sed -n -e '\''/^[^#]/,$p'\'')'''.format(nproc, path)
+            command += r''' <(pbgzip -dc -n {} {} | sed -n -e '\''/^[^#]/,$p'\'')'''.format(npzipin, path)
         else:
             command += r''' <(sed -n -e '\''/^[^#]/,$p'\'' {})'''.format(path)
     command += "'"
