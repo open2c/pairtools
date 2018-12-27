c15e46ac515a1427207f8cc589c4941f3b9ffb63
golobor <goloborodko.anton@gmail.com>
2018-12-26 23:33:19 -0500

    revert dropping sphinx-click

--- doc/conf.py
+++ doc/conf.py
@@ -29,8 +29,8 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = ['sphinx_click.ext']
-extensions = []
+extensions = ['sphinx_click.ext']
+#extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
