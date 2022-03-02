a9e8321ea06b157f5de4d692055fadaed24c80d2
Phlya <flyamer@gmail.com>
2022-03-02 14:02:00 +0100

    Chunked stats

--- pairtools/pairtools_stats.py
+++ pairtools/pairtools_stats.py
@@ -5,6 +5,7 @@ import sys
 import click
 
 import numpy as np
+import pandas as pd
 
 from collections.abc import Mapping
 
@@ -65,26 +66,30 @@ def stats_py(input_path, output, merge, **kwargs):
     )
 
     header, body_stream = _headerops.get_header(instream)
+    cols = _headerops.extract_column_names(header)
 
     # new stats class stuff would come here ...
     stats = PairCounter()
 
     # Collecting statistics
-    for line in body_stream:
-        cols = line.rstrip().split(_pairsam_format.PAIRSAM_SEP)
 
-        # algn1:
-        chrom1 = cols[_pairsam_format.COL_C1]
-        pos1 = int(cols[_pairsam_format.COL_P1])
-        strand1 = cols[_pairsam_format.COL_S1]
-        # algn2:
-        chrom2 = cols[_pairsam_format.COL_C2]
-        pos2 = int(cols[_pairsam_format.COL_P2])
-        strand2 = cols[_pairsam_format.COL_S2]
-        # pair type:
-        pair_type = cols[_pairsam_format.COL_PTYPE]
+    for chunk in pd.read_table(body_stream, names=cols, chunksize=100_000):
+        stats.add_pairs_from_dataframe(chunk)
+    # for line in body_stream:
+    #     cols = line.rstrip().split(_pairsam_format.PAIRSAM_SEP)
 
-        stats.add_pair(chrom1, pos1, strand1, chrom2, pos2, strand2, pair_type)
+    #     # algn1:
+    #     chrom1 = cols[_pairsam_format.COL_C1]
+    #     pos1 = int(cols[_pairsam_format.COL_P1])
+    #     strand1 = cols[_pairsam_format.COL_S1]
+    #     # algn2:
+    #     chrom2 = cols[_pairsam_format.COL_C2]
+    #     pos2 = int(cols[_pairsam_format.COL_P2])
+    #     strand2 = cols[_pairsam_format.COL_S2]
+    #     # pair type:
+    #     pair_type = cols[_pairsam_format.COL_PTYPE]
+
+    #     stats.add_pair(chrom1, pos1, strand1, chrom2, pos2, strand2, pair_type)
 
     # save statistics to file ...
     stats.save(outstream)
@@ -486,7 +491,6 @@ class PairCounter(Mapping):
         self._stat["cis_10kb+"] += int(np.sum(dist >= 10000))
         self._stat["cis_20kb+"] += int(np.sum(dist >= 20000))
         self._stat["cis_40kb+"] += int(np.sum(dist >= 40000))
-        print(self._stat["cis_1kb+"])
 
     def __add__(self, other):
         # both PairCounter are implied to have a list of common fields:
