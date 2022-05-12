0a7ed2f6cfa0019cd44dd3fb9eed1c993d3011b8
Phlya <flyamer@gmail.com>
2022-05-13 00:46:34 +0200

    Start fixing old format...

--- pairtools/lib/stats.py
+++ pairtools/lib/stats.py
@@ -574,13 +574,13 @@ class PairCounter(Mapping):
         else:
             return self.__add__(other)
 
-    def flatten(self):
+    def flatten(self, filter="no_filter"):
         """return a flattened dict (formatted same way as .stats file)"""
         # dict for flat store:
         flat_stat = {}
 
         # Storing statistics
-        for k, v in self._stat.items():
+        for k, v in self._stat[filter].items():
             if isinstance(v, int):
                 flat_stat[k] = v
             # store nested dicts/arrays in a context dependet manner:
@@ -627,7 +627,7 @@ class PairCounter(Mapping):
         # return flattened dict
         return flat_stat
 
-    def format(self):
+    def format_yaml(self):
         """return a formatted dict (for the yaml output)"""
 
         from copy import deepcopy
@@ -732,9 +732,10 @@ class PairCounter(Mapping):
         if yaml:
             import yaml
 
-            data = self.format()
-            yaml.dump(data, outstream, default_flow_style=False)
+            data = self.format_yaml()
+            yaml.dump(data, outstream, default_flow_style=False, sort_keys=False)
         else:
+            data = self.flatten()
             for k, v in self.flatten().items():
                 outstream.write("{}{}{}\n".format(k, self._SEP, v))
 
