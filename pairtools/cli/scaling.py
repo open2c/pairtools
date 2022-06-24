f00dee792b88015a4f791f51b43699cf495b0d98
Aleksandra Galitsyna <agalitzina@gmail.com>
2022-06-23 22:42:25 -0400

    scaling cli viewframe import fix

--- pairtools/cli/scaling.py
+++ pairtools/cli/scaling.py
@@ -87,7 +87,7 @@ def scaling_py(input_path, output, view, chunksize, dist_range, n_dist_bins, **k
     # Pass the header to the instream so that it can parse the header automatically
     cis_scalings, trans_levels = compute_scaling(
         instream,
-        regions=view,
+        regions=pd.read_table(view),
         chromsizes=None,
         dist_range=dist_range,
         n_dist_bins=n_dist_bins,
