269717c119cd5dd47cfbc22322702f37e074e173
Phlya <flyamer@gmail.com>
2022-09-09 16:10:23 +0200

    Don't write index to output

--- pairtools/cli/scaling.py
+++ pairtools/cli/scaling.py
@@ -99,7 +99,7 @@ def scaling_py(input_path, output, view, chunksize, dist_range, n_dist_bins, **k
     summary_stats = pd.concat([cis_scalings, trans_levels])
 
     # save statistics to the file
-    summary_stats.to_csv(outstream, sep="\t")
+    summary_stats.to_csv(outstream, sep="\t", index=False)
 
     if instream != sys.stdin:
         instream.close()
