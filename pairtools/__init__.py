2ada87c43f1aecd67e5919ae6675e0a9be614914
Ilya Flyamer <flyamer@gmail.com>
2023-11-20 20:30:21 +0100

    Bump version

--- pairtools/__init__.py
+++ pairtools/__init__.py
@@ -4,12 +4,12 @@ pairtools
 
 CLI tools to process mapped Hi-C data
 
-:copyright: (c) 2017-2022 Open2C
+:copyright: (c) 2017-2023 Open2C
 :author: Open2C
 :license: MIT
 
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 # from . import lib
