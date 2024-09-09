548b8e1b424494b1fda11fc31255f0303e9393b4
Anton Goloborodko <goloborodko.anton@gmail.com>
2024-09-09 21:28:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ from setuptools import find_packages, setup
 from setuptools.extension import Extension
 
 
-import Cython
 try:
     from Cython.Distutils import build_ext as _build_ext
     from Cython.Build import cythonize
