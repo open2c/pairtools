7c3816969c29f60aed5509d22d585dd1a1ef4e1d
Reovirus <pitikovegor@gmail.com>
2025-04-29 11:32:24 +0200

    add some hints

--- pairtools/lib/fileio.py
+++ pairtools/lib/fileio.py
@@ -9,12 +9,18 @@ class ParseError(Exception):
     pass
 
 
+# dictionary of allowed automatic opener formats and modes 
+# key is format, value is list with allowed modes
 MODES_TO_FILES_PRESET = {
     'bam': ['w', 'r'],
     'gz': ['w', 'r', 'a'],
     'lz4': ['w', 'r', 'a'],
 }
 
+# dictionary of automatic opener commands
+# key is tuple from fomat and mode and value is dictionary 
+# with keys tools - tool which we will try to find via shutil and command which will be formatted by thread number
+#WE NEED COVER
 COMMANDS = {
     ('bam', 'w'): [
         {'tool': 'samtools', 'command': 'samtools view -bS {} -'}
@@ -51,6 +57,21 @@ COMMANDS = {
 
 @dataclass
 class CommandRunResult:
+    """
+    CommandRunResult represents the outcome of executing a command, encapsulating the standard input, output, and error streams, along with the mode of operation.
+
+    Attributes:
+        errors (Optional[TextIO]): The standard error stream (stderr) of the process. Can be None if not applicable.
+        output (Optional[TextIO]): The standard output stream (stdout) of the process. Can be None if not applicable.
+        input (Optional[TextIO]): The standard input stream (stdin) of the process. Can be None if not applicable.
+        mode (Optional[Literal['r', 'w', 'a']]): The mode in which the file is opened: 'r' for reading, 'w' for writing, or 'a' for appending.
+
+    Properties:
+        outfile (Optional[TextIO]): Returns the appropriate file-like object based on the mode:
+            - 'r': Returns the output stream (stdout) for reading.
+            - 'w' or 'a': Returns the input stream (stdin) for writing or appending.
+    """
+
     errors: tp.Optional[tp.TextIO]
     output: tp.Optional[tp.TextIO]
     input: tp.Optional[tp.TextIO]
@@ -59,13 +80,26 @@ class CommandRunResult:
     @property
     def outfile(self) -> tp.Optional[tp.TextIO]:
         if self.mode=='r':
-            return self.input
-        if self.mode=='w' or self.mode=='a':
             return self.output
+        if self.mode=='w' or self.mode=='a':
+            return self.input
 
 
 @dataclass
 class CommandFormatter():
+    """
+    A class to manage file opening operations with support for various compression formats.
+
+    Attributes:
+        mode (str): Mode in which the file is to be opened ('r', 'w', or 'a').
+        path (Optional[str]): Path to the target file.
+        command (Optional[Union[List[str], str]]): Custom command for file processing.
+        nproc (int): Number of threads for multithreaded tools. Defaults to 1.
+        is_binary (bool): Indicates if the file should be opened in binary mode. Defaults to False.
+
+    Methods:
+        __call__(): Executes the command or opens the file based on the provided parameters. Return pairtools.lib.fileio.CommandRunResult object
+    """
 
     mode: tp.Literal['r', 'w', 'a']
     path: tp.Optional[str]=None
@@ -73,6 +107,8 @@ class CommandFormatter():
     nproc: int=1
     is_binary: bool=False
 
+    #Error textm formatter. If some bash tools is needed but not found, user catch error like SomeTool not found, cannot compress output
+    #This method is used to construct understandable error text
     @staticmethod
     def form_notfounderror_text(searched_tools: tp.List[str], is_read: bool) -> str:
         tools_article = 'is' if len(searched_tools) == 1 else 'are'
@@ -87,17 +123,23 @@ class CommandFormatter():
         else:
             self.file_mode = self.mode
 
+        #if we have command, just run it
         if self.command:
             return
         
         self.__format = self.path.split('.')[-1]
 
+        #if format not in MODES_TO_FILES_PRESET, we will return opened file in given mode
         if self.__format not in MODES_TO_FILES_PRESET.keys():
             self.__nocommand = True
             return
+        
+        #cannot open given format in given mode
         if (self.__format, self.mode) not in COMMANDS.keys():
             raise ValueError(f'{self.__format} can not to be opened in {self.mode}')
         
+        #iterate over tools. Function calling ends when tool is founded
+        #just forms self.command
         checked_tools = []
         for possible_solution in COMMANDS[(self.__format, self.mode)]:
             if shutil.which(possible_solution['tool']) is None:
@@ -106,6 +148,7 @@ class CommandFormatter():
             self.command = possible_solution['command'].format(str(self.nproc))
             return
 
+        #if we have no return (in iterate over tools), catch an error
         raise ValueError(self.form_notfounderror_text(checked_tools, self.mode=='r'))
     
     def __convert_command(self):
@@ -122,20 +165,38 @@ class CommandFormatter():
             cmd=subprocess.Popen(self.__command_to_sp, stdout=self.__process_file, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         return cmd
     
-    def __call__(self):
+    def __call__(self) -> CommandRunResult:
         if not self.path or self.path == "-":
             return CommandRunResult(input=sys.stdin, errors=None, output=sys.stdout, mode=self.mode)
         if self.__nocommand:
             self.__process_file = open(self.path, self.file_mode)
-            if self.mode=='r':
-                return CommandRunResult(input=self.__process_file, errors=None, output=None, mode=self.mode)
-            return CommandRunResult(input=None, errors=None, output=self.__process_file, mode=self.mode)
+            return CommandRunResult(input=self.__process_file, errors=None, output=self.__process_file, mode=self.mode)
         self.__convert_command()
         process = self.__form_command()
         return CommandRunResult(input=process.stdin, errors=process.stderr, output=process.stdout, mode=self.mode)
 
 
 def auto_open(path, mode, nproc=1, command=None):
+    """
+    Automatically opens a file based on its format and access mode.
+
+    Determines the file format from its extension and selects the appropriate
+    command for reading or writing, utilizing available compression or
+    decompression tools. If a specific command is provided, it will be used
+    directly.
+
+    Parameters:
+        path (str): Path to the file or '-' for standard input/output.
+        mode (str): File access mode: 'r' (read), 'w' (write), or 'a' (append).
+        nproc (int, optional): Number of threads for multithreaded tools. Defaults to 1.
+        command (str or list, optional): Custom command for file processing. Defaults to None.
+
+    Returns:
+        file-like object: A file object ready for reading or writing data.
+
+    Raises:
+        ValueError: If the file format is unsupported or the required tool is not found.
+    """
     command = CommandFormatter(mode=mode, path=path, command=command, nproc=nproc)
     result = command()
     return result.outfile
